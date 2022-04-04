c6772c4cf7f035b666e362f7a691e2adabbfa0bd
Alvin <alvinmartindwrk@gmail.com>
2022-04-04 15:51:26 +0700

    add static data model

---
+++ lib/model/gallery_data.dart
@@ -0,0 +1,64 @@
+class GalleryPhoto {
+  String name;
+  String description;
+  String imageUrl;
+
+  GalleryPhoto({
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+  });
+}
+
+var galleryPhotoList = [
+  GalleryPhoto(
+    name: 'my laptop',
+    description: 'this is my laptop',
+    imageUrl: 'https://picsum.photos/id/0/300/300',
+  ),
+  GalleryPhoto(
+    name: 'water drip',
+    description: 'rainy day',
+    imageUrl: 'https://picsum.photos/id/123/300/300',
+  ),
+  GalleryPhoto(
+    name: 'old house',
+    description: 'this is old house',
+    imageUrl: 'https://picsum.photos/id/78/300/300',
+  ),
+  GalleryPhoto(
+    name: 'sunrise',
+    description: 'watch out for the sun rising on the horizon',
+    imageUrl: 'https://picsum.photos/id/151/300/300',
+  ),
+  GalleryPhoto(
+    name: 'grape',
+    description: 'this is a grape',
+    imageUrl: 'https://picsum.photos/id/75/300/300',
+  ),
+  GalleryPhoto(
+    name: 'a cup of coffee',
+    description: 'drink this coffee',
+    imageUrl: 'https://picsum.photos/id/30/300/300',
+  ),
+  GalleryPhoto(
+    name: 'lion nose',
+    description: 'this is a lion nose close to the camera',
+    imageUrl: 'https://picsum.photos/id/40/300/300',
+  ),
+  GalleryPhoto(
+    name: 'flower',
+    description: 'this is a flower',
+    imageUrl: 'https://picsum.photos/id/33/300/300',
+  ),
+  GalleryPhoto(
+    name: 'roof top',
+    description: 'this is a roof top',
+    imageUrl: 'https://picsum.photos/id/101/300/300',
+  ),
+  GalleryPhoto(
+    name: 'glasses',
+    description: 'see the world through the glasses',
+    imageUrl: 'https://picsum.photos/id/201/300/300',
+  ),
+];
